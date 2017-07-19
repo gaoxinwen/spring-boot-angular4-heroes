138caa7d2f4f80faad65d4b64c09dc311f7c1af4
gaoxinwen <gaoxinwen00@gmail.com>
2017-07-19 09:52:55 +0800

    Use RequestOptions instead.

--- frontend/src/main/frontend/src/app/hero-search.service.ts
+++ frontend/src/main/frontend/src/app/hero-search.service.ts
@@ -1,7 +1,7 @@
 import 'rxjs/add/operator/map';
 
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import { Hero } from './hero';
@@ -9,14 +9,16 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroSearchService {
 
-  private heroesUrl = 'http://localhost:8080/api/heroes';  // URL to web api
+  private searchUrl = 'http://localhost:8080/api/heroes/search';  // URL to web api
 
   constructor(private http: Http) { }
 
   search(term: string): Observable<Hero[]> {
-    console.log(term);
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('name', term);
+    // search is deprecated from 4.0.0. Use params instead.
     return this.http
-      .get(`${this.heroesUrl}/search/?name=${term}`)
+      .get(this.searchUrl, { params: params })
       .map(response => response.json() as Hero[]);
   }
 }
